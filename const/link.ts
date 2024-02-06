4392a4cbd8b3c5d0f48ce43b245cf4a783435a95
Tahara Kazuki <>
2024-02-07 02:06:48 +0900

    update

---
+++ const/link.ts
@@ -0,0 +1,22 @@
+export const MENU_ITEMS = [
+  {
+    url: "/",
+    title: "ホーム",
+    subTitle: "Home",
+  },
+  {
+    url: "/about",
+    title: "私たちについて",
+    subTitle: "About",
+  },
+  {
+    url: "/daily-schedule",
+    title: "1日の流れ",
+    subTitle: "Daily Schedule",
+  },
+  {
+    url: "/contact",
+    title: "お問い合わせ",
+    subTitle: "Contact",
+  },
+]
