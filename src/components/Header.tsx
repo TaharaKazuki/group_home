71ec128d8385bdc8a9215b96594e8558fa316c91
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-30 13:02:51 +0900

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,22 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Logo from './img/header/logo.svg';
-// import Socials from './Socials';
-// import MobileNav from './MobileNav';
+import Socials from './Socials';
+import MobileNav from './MobileNav';
 
 const Header = () => {
   return (
     <div className="bg-pink-200 fixed w-full px-[30px] lg:px-[100px] h-[100px] lg:h-[140px] flex items-center">
       <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
         <Link href="/">
-          <div className="h-[100px] w-[200px] flex justify-center items-center">
-            <Image src={Logo} alt="Logo" />
+          <div className="h-[100px] w-[200px] flex justify-center items-center overflow-hidden">
+            <Image
+              src={Logo}
+              alt="Logo"
+              width={200}
+              height={200}
+              objectFit="contain"
+            />
           </div>
         </Link>
       </div>
