7974ad0f80bab812c15494ebdd5ab41114a6c347
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-31 12:11:11 +0900

    update

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,43 +1,59 @@
 import { FaXTwitter, FaInstagram, FaFacebookF } from 'react-icons/fa6';
 import Link from 'next/link';
+import { text } from 'stream/consumers';
+
+const socialLinks = [
+  {
+    href: 'https://www.facebook.com/',
+    icon: (
+      <FaFacebookF className="text-xl text-blue-600 group-hover:text-white" />
+    ),
+    hoverColor: 'group-hover:bg-blue-600',
+    text: 'Facebook',
+    textColor: 'text-blue-600',
+  },
+  {
+    href: 'https://x.com/?lang=ja',
+    icon: (
+      <FaXTwitter className="text-xl text-blue-400 group-hover:text-white" />
+    ),
+    hoverColor: 'group-hover:bg-blue-400',
+    text: 'X(Twitter)',
+    textColor: 'text-blue-400',
+  },
+  {
+    href: 'https://www.instagram.com/',
+    icon: (
+      <FaInstagram className="text-xl text-pink-600 group-hover:text-white" />
+    ),
+    hoverColor: 'group-hover:bg-pink-600',
+    text: 'Instagram',
+    textColor: 'text-pink-600',
+  },
+];
 
 const Socials = () => {
   return (
     <div className="hidden xl:flex ml-24">
       <div className="wrapper flex space-x-2">
-        <Link
-          href="https://www.facebook.com/"
-          className="button group flex items-center"
-        >
-          <div className="icon bg-white group-hover:bg-blue-600 flex items-center justify-center">
-            <FaFacebookF className="text-xl text-blue-600 group-hover:text-white" />
-          </div>
-          <span className="hidden group-hover:inline-block ml-2 text-blue-600">
-            Facebook
-          </span>
-        </Link>
-        <Link
-          href="https://x.com/?lang=ja"
-          className="button group flex items-center"
-        >
-          <div className="icon bg-white group-hover:bg-blue-400 flex items-center justify-center">
-            <FaXTwitter className="text-xl text-blue-400 group-hover:text-white" />
-          </div>
-          <span className="hidden group-hover:inline-block ml-2 text-blue-400">
-            X(Twitter)
-          </span>
-        </Link>
-        <Link
-          href="https://www.instagram.com/"
-          className="button group flex items-center"
-        >
-          <div className="icon bg-white group-hover:bg-pink-600 flex items-center justify-center">
-            <FaInstagram className="text-xl text-pink-600 group-hover:text-white" />
-          </div>
-          <span className="hidden group-hover:inline-block ml-2 text-pink-600">
-            Instagram
-          </span>
-        </Link>
+        {socialLinks.map((link, index) => (
+          <Link
+            key={index}
+            href={link.href}
+            className="button group flex items-center"
+          >
+            <div
+              className={`icon bg-white ${link.hoverColor} flex items-center justify-center`}
+            >
+              {link.icon}
+            </div>
+            <span
+              className={`hidden group-hover:inline-block ml-2 ${link.textColor}`}
+            >
+              {link.text}
+            </span>
+          </Link>
+        ))}
       </div>
     </div>
   );
