6b59a447ac59394bce41ab626f3cf8d4ad1c5099
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-31 10:36:38 +0900

    update

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -5,16 +5,43 @@ import Link from 'next/link';
 
 import { motion } from 'framer-motion';
 
+const menuVariants = {
+  hidden: {
+    x: '100%',
+  },
+  show: {
+    x: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.9],
+    },
+  },
+};
+
 const MobileNav = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
-    <nav>
-      <div className="text-3xl cursor-pointer">
+    <nav className="text-primary xl:hidden">
+      <div
+        onClick={() => setOpenMenu(true)}
+        className="text-3xl cursor-pointer"
+      >
         <CgMenuRight />
       </div>
-      <div className="bg-white shadow-2xl w-full absolute h-screen top-0 right-0 max-w-xs z-20">
-        <div>icon</div>
+      <motion.div
+        variants={menuVariants}
+        initial="hidden"
+        animate={openMenu ? 'show' : ''}
+        className="bg-white shadow-2xl w-full absolute h-screen top-0 right-0 max-w-xs z-20"
+      >
+        <div
+          onClick={() => setOpenMenu(false)}
+          className="text-4xl absolute z-30 left-4 top-10 text-primary cursor-pointer"
+        >
+          <IoMdClose />
+        </div>
 
-        <ul className="h-full flex flex-col justify-center items-start gap-y-8 text-primary font-primary font-bold text-3xl">
+        <ul className="h-full flex flex-col justify-center items-start gap-y-8 text-primary  relative left-4 font-primary font-bold text-3xl">
           <li>
             <Link
               href="/"
@@ -44,7 +71,7 @@ const MobileNav = () => {
             </Link>
           </li>
         </ul>
-      </div>
+      </motion.div>
     </nav>
   );
 };
