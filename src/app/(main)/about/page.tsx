69350480f42da48527d82a0c0564a98145907013
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-31 14:18:35 +0900

    update

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -1,12 +1,39 @@
 import Image from 'next/image';
+import AboutImage from '../../../components/img/about/about.jpg';
+import Link from 'next/link';
 
 const AboutPage = () => {
   return (
     <section className="section">
       <div className="container mx-auto h-full relative">
-        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
-          <div>Image</div>
-          <div>Text</div>
+        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left">
+          <div className="flex-1 max-h-72 lg:max-h-max order-2 lg:order-none overflow-hidden">
+            <Image src={AboutImage} alt="sample" />
+          </div>
+          <div className="flex-1 pt-10 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start">
+            <h1 className="text-2xl">私たちについて</h1>
+            <p>
+              サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+              サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+              サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+              サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+              サンプルサンプルサンプルサンプルサンプル
+            </p>
+            <br />
+            <br />
+            <p>
+              サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+              サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+              サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+              サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+              サンプルサンプルサンプルサンプルサンプル
+            </p>
+            <br />
+            <br />
+            <Link className="btn" href="/portfolio">
+              Portfolio
+            </Link>
+          </div>
         </div>
       </div>
     </section>
