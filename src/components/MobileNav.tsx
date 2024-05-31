791d4e3e1d02ae9de0a873bfd24d3331cce7dbab
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-31 10:27:10 +0900

    update

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -11,6 +11,40 @@ const MobileNav = () => {
       <div className="text-3xl cursor-pointer">
         <CgMenuRight />
       </div>
+      <div className="bg-white shadow-2xl w-full absolute h-screen top-0 right-0 max-w-xs z-20">
+        <div>icon</div>
+
+        <ul className="h-full flex flex-col justify-center items-start gap-y-8 text-primary font-primary font-bold text-3xl">
+          <li>
+            <Link
+              href="/"
+              className="flex items-center gap-x-2
+            "
+            >
+              <p>Home</p>
+              <span className="text-xs">ホーム</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="flex items-center gap-x-2">
+              <p>About</p>
+              <span className="text-xs">私たちについて</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/Portfolio" className="flex items-center gap-x-2">
+              <p>Portfolio</p>
+              <span className="text-xs">日常の流れ</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="flex items-center gap-x-2">
+              <p>Contact</p>
+              <span className="text-xs">お問い合わせ</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
