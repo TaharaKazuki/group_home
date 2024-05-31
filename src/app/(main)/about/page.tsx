43cce72ad867bb8aba9508864318d7900630d6d4
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-31 15:37:38 +0900

    update

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -6,15 +6,18 @@ const AboutPage = () => {
   return (
     <section className="section">
       <div className="container mx-auto h-full relative">
-        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
-          <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none">
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-x-24 text-center lg:text-left">
+          <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden">
             <Image src={AboutImage} alt="sample" />
           </div>
-          <div className="flex-1 pt-36 pb-14 px-2 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start">
+          <div className="flex-1 pt-12 pb-14 px-2 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start">
             <h1 className="text-4xl font-bold capitalize leading-[120%] tracking-[-0.05em] mb-2">
               私たちについて
             </h1>
             <p className="mb-12 max-w-full">
+              サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+              サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+              サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
               サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
               サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
               サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
     <section className="section">
       <div className="container mx-auto h-full relative">
         <div className="flex flex-col justify-center">
-          <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute lg:bottom-32 flex flex-col justify-center items-center lg:items-start">
+          <div className="w-full pt-12 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute lg:top-52 flex flex-col justify-center items-center lg:items-start">
             <h1 className="h1">IXIA</h1>
             <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
               GROUP HOME
