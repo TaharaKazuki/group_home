435de90c1afe5707b4ee2a153051d0e554b3332f
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-31 12:08:34 +0900

    update

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -5,14 +5,39 @@ const Socials = () => {
   return (
     <div className="hidden xl:flex ml-24">
       <div className="wrapper flex space-x-2">
-        <div className="button group">
-          <div className="icon bg-white group-hover:bg-blue-600">
-            <FaFacebookF className="text-2xl text-blue-600" />
+        <Link
+          href="https://www.facebook.com/"
+          className="button group flex items-center"
+        >
+          <div className="icon bg-white group-hover:bg-blue-600 flex items-center justify-center">
+            <FaFacebookF className="text-xl text-blue-600 group-hover:text-white" />
           </div>
           <span className="hidden group-hover:inline-block ml-2 text-blue-600">
             Facebook
           </span>
-        </div>
+        </Link>
+        <Link
+          href="https://x.com/?lang=ja"
+          className="button group flex items-center"
+        >
+          <div className="icon bg-white group-hover:bg-blue-400 flex items-center justify-center">
+            <FaXTwitter className="text-xl text-blue-400 group-hover:text-white" />
+          </div>
+          <span className="hidden group-hover:inline-block ml-2 text-blue-400">
+            X(Twitter)
+          </span>
+        </Link>
+        <Link
+          href="https://www.instagram.com/"
+          className="button group flex items-center"
+        >
+          <div className="icon bg-white group-hover:bg-pink-600 flex items-center justify-center">
+            <FaInstagram className="text-xl text-pink-600 group-hover:text-white" />
+          </div>
+          <span className="hidden group-hover:inline-block ml-2 text-pink-600">
+            Instagram
+          </span>
+        </Link>
       </div>
     </div>
   );
