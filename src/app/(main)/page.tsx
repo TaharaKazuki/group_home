740e88b0c296544f4ad5576d7960bdf172a74fa4
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-06-01 12:23:33 +0900

    update

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -11,43 +11,43 @@ import { transition1 } from '@/util/transitions';
 
 const Home = () => {
   return (
-    <MotionWrapper>
-      <motion.section
-        className="section"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={transition1}
-      >
-        <div className="container h-full mx-auto min-h-screen relative lg:overflow-hidden overflow-y-auto">
-          <div className="flex flex-col justify-center">
+    // <MotionWrapper>
+    <motion.section
+      className="section"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={transition1}
+    >
+      <div className="container h-full mx-auto min-h-screen relative lg:overflow-hidden overflow-y-auto">
+        <div className="flex flex-col justify-center">
+          <motion.div
+            initial={{ opacity: 0, y: '-50%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '-50%' }}
+            transition={transition1}
+            className="w-full pt-24 pb-2 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute lg:top-[400px] flex flex-col justify-center items-center lg:items-center"
+          >
+            <Image src={HomeLogo} alt="" objectFit="contain" />
+            <Link href="/contact" className="btn mt-5 mb-[30px]">
+              お問い合わせはこちら
+            </Link>
+          </motion.div>
+          <div className="flex justify-end max-h-screen lg:max-h-max">
             <motion.div
-              initial={{ opacity: 0, y: '-50%' }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: '-50%' }}
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
               transition={transition1}
-              className="w-full pt-24 pb-2 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute lg:top-[400px] flex flex-col justify-center items-center lg:items-center"
+              className="relative lg:-right-40 lg:-top-[350px]"
             >
-              <Image src={HomeLogo} alt="" objectFit="contain" />
-              <Link href="/contact" className="btn mt-5 mb-[30px]">
-                お問い合わせはこちら
-              </Link>
+              <Image src={FlowerImg} alt="sample" objectFit="contain" />
             </motion.div>
-            <div className="flex justify-end max-h-screen lg:max-h-max">
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                exit={{ scale: 0 }}
-                transition={transition1}
-                className="relative lg:-right-40 lg:-top-[350px]"
-              >
-                <Image src={FlowerImg} alt="sample" objectFit="contain" />
-              </motion.div>
-            </div>
           </div>
         </div>
-      </motion.section>
-    </MotionWrapper>
+      </div>
+    </motion.section>
+    // </MotionWrapper>
   );
 };
 
