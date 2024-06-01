96865894f4cf118d5eb162d350e65b7d3117f173
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-06-01 14:41:15 +0900

    update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,20 @@
 'use client';
 
 import Image from 'next/image';
-import { useTransform, motion, useScroll, MotionValue } from 'framer-motion';
+import {
+  useTransform,
+  motion,
+  useScroll,
+  MotionValue,
+  progress,
+} from 'framer-motion';
 import { useRef } from 'react';
 import Sample from '../components/img/schedule/sample.jpg';
 
 type CardProps = {
   i: number;
   title: string;
+  subTitle: string;
   description: string;
   src: string;
   color: string;
@@ -17,22 +24,15 @@ type CardProps = {
 };
 
 const Card = ({
-  i,
   title,
+  subTitle,
   description,
   src,
-  color,
   progress,
   range,
   targetScale,
 }: CardProps) => {
   const container = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: container,
-    offset: ['start end', 'start start'],
-  });
-
-  const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1]);
   const scale = useTransform(progress, range, [1, targetScale]);
 
   return (
@@ -42,26 +42,28 @@ const Card = ({
     >
       <motion.div
         style={{
-          borderColor: color,
-          // scale,
+          scale,
         }}
         className="flex flex-col relative lg:h-[650px] lg:w-[1700px] w-full h-[600px] rounded-md lg:p-[50px] p-3 lg:top-0 top-14 bg-white"
       >
-        <h1 className="text-center m-0 text-2xl font-bold">{title}</h1>
+        <h1 className="text-center m-0 text-3xl font-bold flex items-center flex-col">
+          <p>{title}</p>
+          <span className="text-sm">{subTitle}</span>
+        </h1>
         <div className="flex flex-col lg:flex-row h-full lg:mt-12 lg:gap-12 gap-4">
           <div className="w-full lg:w-1/2 h-full flex flex-col lg:order-1 order-2">
             <p className="lg:text-lg line-clamp-6">{description}</p>
           </div>
-          <div className="relative w-full lg:w-1/2 h-[300px] lg:h-full flex items-center justify-center rounded-md lg:order-2 order-1">
+          <div className="relative w-full lg:w-1/2 h-[300px] lg:h-full flex items-center justify-center lg:order-2 order-1">
             {src && (
-              <div className="w-full h-full">
+              <motion.div className="w-full h-full">
                 <Image
                   fill
                   src={`/images/schedule/${src}`}
                   alt="image"
-                  className="object-cover"
+                  className="object-cover rounded-md"
                 />
-              </div>
+              </motion.div>
             )}
           </div>
         </div>
