7c1ea222106b301ec7bbe5e9f968db09cfbe8d29
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-09-11 21:34:36 +0900

    トースターコンポーネントのテーマを"system"から"light"に変更し、トーストのスタイルを調整。成功とエラーのトーストに新しいクラス名を追加し、視覚的な一貫性を向上。

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,30 +1,28 @@
 "use client"
 
-import { useTheme } from "next-themes"
 import { Toaster as Sonner } from "sonner"
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
-
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme="light"
       className="toaster group"
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-white group-[.toaster]:text-gray-900 group-[.toaster]:border-gray-200 group-[.toaster]:shadow-xl group-[.toaster]:text-lg group-[.toaster]:p-6",
-          description: "group-[.toast]:text-gray-700 group-[.toast]:text-base",
+            "group toast group-[.toaster]:bg-white group-[.toaster]:text-gray-700 group-[.toaster]:border group-[.toaster]:border-red-300 group-[.toaster]:shadow-xl group-[.toaster]:text-lg group-[.toaster]:p-6 group-[.toaster]:font-bold",
+          description:
+            "group-[.toast]:text-gray-700 group-[.toast]:text-base group-[.toast]:font-normal",
           actionButton:
             "group-[.toast]:bg-gray-900 group-[.toast]:text-gray-50",
           cancelButton:
             "group-[.toast]:bg-gray-100 group-[.toast]:text-gray-500",
           success:
-            "group-[.toaster]:bg-green-100 group-[.toaster]:text-green-800 group-[.toaster]:border-green-300 group-[.toaster]:font-semibold",
+            "group-[.toaster]:bg-white group-[.toaster]:text-gray-700 group-[.toaster]:border group-[.toaster]:border-red-300 group-[.toaster]:font-bold [&>svg]:text-red-300",
           error:
-            "group-[.toaster]:bg-red-100 group-[.toaster]:text-red-800 group-[.toaster]:border-red-300 group-[.toaster]:font-semibold",
+            "group-[.toaster]:bg-white group-[.toaster]:text-gray-700 group-[.toaster]:border group-[.toaster]:border-red-300 group-[.toaster]:font-bold [&>svg]:text-red-300",
         },
       }}
       {...props}
